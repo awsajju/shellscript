9826bcea1d1e907ce474e0a8a3983090159aa060
awsajju <aws.devops.123aws@gmail.com>
2025-01-03 16:24:11 +0530

    installing java

--- java-install.sh
+++ java-install.sh
@@ -8,15 +8,18 @@ echo "ERROR: you must have the sudo access to execute the script"
 exit 1
 fi 
 
-dnf install java -y
-
+dnf list installed java
 if [ $? -ne 0 ]; then
 
-echo "Installing java is fialure"
-exit 1
-else
-echo installing java is success"
+ dnf install java -y
+
+    if [ $? -ne 0 ]; then
+
+        echo "Installing java is fialure"
+        exit 1
+        else
+        echo "installing java is success"
+        fi
+else 
+echo "java already installed"
 fi
-
-
-dnf install mysql -y
