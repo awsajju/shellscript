b15a53bb23419aff92814b246698d3b64ace21d8
awsajju <aws.devops.123aws@gmail.com>
2025-01-03 15:27:53 +0530

    arrays

--- array.sh
+++ array.sh
@@ -7,4 +7,4 @@ echo "2nd movie : ${Movie[1]}"
 echo "print all movie :${Movie[@]}"
 
 echo "print all the movie in one string :${Movie[*]}"
-echo "prinit how many varaible are reflecting : ${Movie[#]}"
+echo "prinit how many varaible are reflecting : ${#Movie[@]}"
