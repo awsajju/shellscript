b8cb79a10f083ec5539f09f073db0819a081549e
awsajju <aws.devops.123aws@gmail.com>
2025-01-03 14:25:11 +0530

    passwd

--- pass.sh
+++ pass.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-read -s -p "please Enter you user_name : " user_name
+read -s -p "please Enter you user_name : user_nam"
 
 echo "User Name entered : $user_name"
 
-read -s -p "Please Enter your Passwd : " passwd
+read -s -p "Please Enter your Passwd : passwd"
 
 echo "Passwd Entered : $passwd"
