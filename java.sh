9bcd0a2e8e11c417472d3cdf4d549d73bfe295f6
awsajju <aws.devops.123aws@gmail.com>
2025-01-03 17:05:36 +0530

    installing java

---
+++ java.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID $? 0 ];then
+    echo "you must have the sudo access to execute this"
+    exit 1
+
+fi
+
+dnf list installed java
+
+if [ $? -ne 0 ];then
+
+    dnf install java -y
+        if [ $? -ne 0 ];then
+            echo "installing java is failure"
+            exit 1
+        else 
+            echo "installing java successful"
+        fi
+else
+
+    echo "already installed"
+
+fi
+
+
